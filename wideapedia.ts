7ec559d211854c3ae0238856d402c0c0d76b3414
esummins <esummins@gmail.com>
2023-01-20 20:31:22 -0800

    remove annoying css

---
+++ wideapedia.ts
@@ -0,0 +1,19 @@
+// Offending classes
+let class_names = ['mw-page-container', 'mw-content-container']
+
+// New layout check
+if (document.getElementsByClassName(class_names[0]).length > 0) {
+  class_names.forEach(function(class_name) {
+    // grab element by classname
+    let element = document.getElementsByClassName(class_name)[0]
+
+    // preserve any existing styles
+    if (!element.getAttribute('style')) {
+      element.setAttribute('style', "")
+    }
+    let current_style = element.getAttribute('style')
+
+    // remove max-width
+    element.setAttribute("style", current_style + " max-width: none;")
+  });
+}
